42a2206c7a5c507c87e2b75462e6094658093868
Kevin Yin <kevin.yin3@gmail.com>
2016-12-07 16:52:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,11 @@ function myFunction() {
    var ss = SpreadsheetApp.getActiveSpreadsheet();
  var sheet = ss.getSheets()[0];
 var pos=1;
- /* var cell=sheet.getRange('A2');
-  Logger.log(sheet.getRange('A2').getNote());
-  if (cell.getNote() != ""){
-  cell.setValue(cell.getNote());
-  }*/    
+var row= 218;  
 var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   for (pos;pos<27;pos++){  
     var z = letters.substring(pos-1,pos);
-    for (var i=1;i<218;i++){
+    for (var i=1;i<row;i++){
       var cell = sheet.getRange(z+(i.toString()));
       if (cell.getNote() != ""){
         cell.setValue(cell.getNote());
